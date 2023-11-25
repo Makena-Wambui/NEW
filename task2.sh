c2d0001fa8f60b41246938428f784d456be913b2
iammakena@gmail.com <root@MAKENA>
2023-11-25 13:59:10 +0300

    Shell scripts and Python

--- task2.sh
+++ task2.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+#Problem was that the python interpreter was interpreting the entire string
+#as a file path and trying to open it, which leads to an error.
+#To execute Python code directly from a string stored in an environment 
+#variable, you can use the -c option with the Python interpreter.
+#the -c option allows you to specify the
+#Python code as a command-line argument.
+#double quotes around $PYCODE are necessary 
+#to preserve the spaces and special characters in the Python code.
+
 python3 -c "$PYCODE"
