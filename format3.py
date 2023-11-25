c88bdbf5d46472ae94fc70767a02762357832b1c
Makena-Wambui <iammakena@gmail.com>
2023-11-25 12:48:15 +0300

    Format method

--- format3.py
+++ format3.py
@@ -1,6 +1,8 @@
 message = '{0} is easier than {1}'.format('Python', 'Java')
 message1 = '{1} is easier than {0}'.format('Python', 'Java')
 message2 = '{0:5.2f} and {1:d}'.format(1.23456789, 11)
+message4 = '{}'.format(1.234567)
 print(message)
 print(message1)
 print(message2)
+print(message4)
