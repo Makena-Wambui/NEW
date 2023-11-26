c883719d6cd2fba2bf23b0702ed9fd162adb1202
iammakena@gmail.com <root@MAKENA>
2023-11-26 22:04:32 +0300

    Had issues with Vs

---
+++ makena.py
@@ -0,0 +1,10 @@
+'''
+Ask the user for their name using input
+then store this name in a variable called name
+'''
+
+name = input('Please enter your name ')
+
+# Then print Hello followed by the name of the user
+
+print('Hello ', name)
