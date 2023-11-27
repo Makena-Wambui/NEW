6d18dc762d4b9f02c910c8d1bbf21784a5fe23cd
iammakena@gmail.com <root@MAKENA>
2023-11-27 11:12:01 +0300

    Type casting

---
+++ typecasting.py
@@ -0,0 +1,24 @@
+# 3 built_in functions for typecasting
+num1 = int(2.167)
+print(num1)
+
+num2 = int('2')
+print(num2)
+
+num3 = int(float("2.167"))
+print(num3)
+# Invalid operation. The string literal has to have a base of 10.
+# num  = int('2.167')
+
+a = float(2)
+print(a)
+b = float('2')
+print(b)
+c = float('2.167')
+print(c)
+
+i = str(2)
+print(i)
+
+j = str(2.1)
+print(j)
