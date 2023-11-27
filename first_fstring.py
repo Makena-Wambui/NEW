190a75420028c33312c1e29c6778fb31d5e69dd2
iammakena@gmail.com <root@MAKENA>
2023-11-28 01:35:06 +0300

    f strings

---
+++ first_fstring.py
@@ -0,0 +1,4 @@
+name = "Makena"
+age = 30
+
+print(f"Hello {name}. You are {age} years old today.")
---
+++ formatspecifiers_with_format_method.py
@@ -0,0 +1,4 @@
+# Note diff btwn format specifiers and conversion specifiers
+
+print("Bank Balance: Kshs {:.2f}".format(200.567))
+print("{:=^100}".format('Makena Wambui'))
