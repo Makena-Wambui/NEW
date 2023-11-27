21acc4119fb7c472db3aad89f29d25dff6c72f33
iammakena@gmail.com <root@MAKENA>
2023-11-27 19:40:56 +0300

    More dictionaries

---
+++ modulo3.py
@@ -0,0 +1,3 @@
+# Demonstrate dictionaries
+
+print("Hello %(name)s. You are %(age)d years!" % {"name": "Alicia", "age": 30})
---
+++ modulo4.py
@@ -0,0 +1,3 @@
+# Example 2
+# Dictionaries
+print('is your laptop %(brand)s? Does it cost %(price)d USD?' % {"brand": "Dell", "price": 1000})
