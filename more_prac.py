17581616f2cf217d236561126fcbeec492e3a0d6
iammakena@gmail.com <root@MAKENA>
2023-11-27 21:38:23 +0300

    More formatting

--- more_prac.py
+++ more_prac.py
@@ -2,4 +2,8 @@
 age = 30
 name = "Alicia"
 
+# Using numeric indices
 print("Hello {1}! Are you {0} years old?".format(age, name))
+
+# Use keyword arguments
+print("Hello {name}! Are you {age} years old today?".format(name="Alicia", age=30))
