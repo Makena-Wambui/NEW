f0ad564192e2d380b9088e4f18b9ab4f974ea296
iammakena@gmail.com <root@MAKENA>
2023-11-27 23:01:48 +0300

    More..

--- dictionary_plus_format.py
+++ dictionary_plus_format.py
@@ -1,5 +1,5 @@
 # Using dictionaries with the format method
-
+# use the dictionary unpacking operator **
 person = {"name": "Alicia", 'age': 30}
 
 print("Hello {name}. You are {age} years old.".format(**person))
