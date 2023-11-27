6daf95251b2d8d53c81d9cbd2c5c6dfd8e297c5e
Makena-Wambui <140601003+Makena-Wambui@users.noreply.github.com>
2023-11-27 20:36:23 +0300

    Update single_item_tuple.py

--- single_item_tuple.py
+++ single_item_tuple.py
@@ -1,4 +1,5 @@
 # Drawbacks of using modulo operator for string interpolation and formatting
 # Wrapping the data as a single item tuple
+# Hard to read, understand and remember this syntax
 
 print("Personal info: %s." % (('John', 20),))
