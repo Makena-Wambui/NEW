044168ff0038ee3a2fed21bb1f6f30863ae18bb2
iammakena@gmail.com <root@MAKENA>
2023-11-28 14:18:11 +0300

    If

---
+++ if_2.py
@@ -0,0 +1,7 @@
+userInput = eval(input('Please enter 1 or 2:'))
+if userInput == 1:
+    print('Hello,Welcome to Sprint1')
+elif userInput == 2:
+    print("Hello, welcome to sprint2")
+else:
+    print("Invalid Input")
