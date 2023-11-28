61189e7271f20e321e0503fe491c2a2ab5cd9e92
iammakena@gmail.com <root@MAKENA>
2023-11-28 14:48:26 +0300

    Inline if

---
+++ inline2.py
@@ -0,0 +1,2 @@
+val = eval(input('Choose between 1 and 2: '))
+print("Task A" if val == 1 else "Task B")
---
+++ inline_if.py
@@ -0,0 +1,4 @@
+# To perform a very simple task.
+val = int(input("Choose 1 or 2: "))
+num1 = 12 if val==1 else 13
+print(num1)
