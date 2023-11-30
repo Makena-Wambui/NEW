66d0967baaedd4f9b6a4033d40d0f2a513dcca57
iammakena@gmail.com <root@MAKENA>
2023-11-30 11:45:54 +0300

    name global variable in Python

---
+++ MODULES/alicia.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def hello():
+    print("Hello Alicia.")
+
+
+if __name__ == "__main__":
+    print("Ths script was run as the main program")
+else:
+    print("This script was imported as a module into another script")
