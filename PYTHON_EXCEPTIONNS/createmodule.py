0880b3c4608de62b58791c6bdc5add74c328fd34
iammakena@gmail.com <root@MAKENA>
2023-12-18 11:33:49 +0300

    Creation of Modules and importing them in other modules

---
+++ PYTHON_EXCEPTIONNS/createmodule.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def GetTotal(*args):
+    total = 0
+    for num in args:
+        total += num
+    return total
---
+++ PYTHON_EXCEPTIONNS/module2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import createmodule
+
+print("{}".format(createmodule.GetTotal(3, 4, 5)))
